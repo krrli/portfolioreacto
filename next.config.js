2ced228763794b9c18188ced8c1a02087769783a
Carla Iten <carlaiten@gmail.com>
2022-12-20 09:41:57 +0100

    updated contentful config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: config => {
+    config.plugins = config.plugins || [];
+
+    config.plugins = [
+      ...config.plugins,
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    ];
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
