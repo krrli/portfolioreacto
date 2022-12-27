3bb661d5811885024e23b06ca8776dc80d8ac6dc
Carla Iten <carlaiten@gmail.com>
2022-12-27 16:19:27 +0100

    added react icons, used burger in navbar

--- components/Navbar/NavBar.tsx
+++ components/Navbar/NavBar.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import styles from './Navbar.module.scss';
 import {NavLink} from "react-router-dom";
 import Link from "next/link";
+import { FiMenu } from "react-icons/fi";
+
+
 
 
 
@@ -13,8 +16,8 @@ const Navbar = () => {
                 <div className={styles.navContainer}>
 
                     <Link href="/"><h1>Carla Iten</h1></Link>
-
                     <div className={styles.nav}>
+                        <FiMenu className={styles.navBurger} />
                         <Link className={styles.navItem} href="/cv">CV</Link>
                         <Link className={styles.navItem} href="/skills">Skills</Link>
                         <Link className={styles.navItem} href="/projects">Projekte</Link>
